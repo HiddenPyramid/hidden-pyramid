eaad83df88aca73bab6a933096d13cff189f3a82
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-05-11 23:00:46 +0200

    FOR LATER - Added scripts to spawn/despawn corridors and optimize

---
+++ 2D/Assets/Scripts/Environment/LevelDespawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))] 
+public class LevelDespawner : MonoBehaviour
+{
+    // Intended usage: attack to object, make prefab and:
+    // (Optional) 1 Place Level Spawner for next level
+    // 2 Place Level Spawner for previous level (in case player goes back)
+    // 3 Place Level DESpawner for previous level
+
+    public GameObject level;
+    
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Despawn();
+        }
+    }
+
+    private void Despawn()
+    {
+        Destroy(level);
+    }
+}
