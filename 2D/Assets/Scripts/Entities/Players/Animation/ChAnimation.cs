e4ddba5a7b84a18bc9045a2687e60e9f79df703a
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-05-21 11:36:35 +0200

    Character shadow initial implementation

--- 2D/Assets/Scripts/Entities/Players/Animation/ChAnimation.cs
+++ 2D/Assets/Scripts/Entities/Players/Animation/ChAnimation.cs
@@ -8,6 +8,8 @@ public class ChAnimation : MonoBehaviour
     [SerializeField]
     private Animator animator;
     [SerializeField]
+    private Animator shadowAnimator;
+    [SerializeField]
     private Transform visuals;
     public Transform slymVisuals;
 
@@ -28,26 +30,33 @@ public class ChAnimation : MonoBehaviour
         if (input > 0)
         {
             animator.SetBool(Parameter.ANIM_RUNNING, true);
+            shadowAnimator.SetBool(Parameter.ANIM_RUNNING, true);
             visuals.localScale = Vector3.one;
             slymVisuals.localScale = Vector3.one;
         }
         else if(input < 0)
         {
             animator.SetBool(Parameter.ANIM_RUNNING, true);
+            shadowAnimator.SetBool(Parameter.ANIM_RUNNING, true);
             visuals.localScale = new Vector3(-1, visuals.localScale.y, visuals.localScale.z);
             slymVisuals.localScale = new Vector3(-1, slymVisuals.localScale.y, slymVisuals.localScale.z);
         }
         else
+        {
             animator.SetBool(Parameter.ANIM_RUNNING, false);
+            shadowAnimator.SetBool(Parameter.ANIM_RUNNING, false);
+        }
     }
 
     public void StartJump()
     {
         animator.SetBool(Parameter.ANIM_JUMPING, true);
+        shadowAnimator.SetBool(Parameter.ANIM_JUMPING, true);
     }
 
     public void EndJump()
     {
         animator.SetBool(Parameter.ANIM_JUMPING, false);
+        shadowAnimator.SetBool(Parameter.ANIM_JUMPING, false);
     }
 }
