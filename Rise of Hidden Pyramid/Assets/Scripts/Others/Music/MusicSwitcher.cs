98c740902d70018c387aca59f37a62800f97de3b
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-06-07 22:57:10 +0200

    Some background Music added And Persistent Skybox updated

--- Rise of Hidden Pyramid/Assets/Scripts/Others/Music/MusicSwitcher.cs
+++ Rise of Hidden Pyramid/Assets/Scripts/Others/Music/MusicSwitcher.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class MusicSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audioSource;
+    public AudioClip nextClip;
+
+    public float smoothTime = 0.7f;
+    float velocity = 0.0f;
+    public float delayTime = 0.3f;
+    public float maxVolume = 1f;
+
+    public bool alreadySwitched = false;
+
+    private void OnTriggerEnter(Collider other) 
     {
-        
+        if (!alreadySwitched &&  other.gameObject.CompareTag(Parameter.PLAYER))
+        {
+            alreadySwitched = true;
+            SwitchMusic();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SwitchMusic()
     {
-        
+        StartCoroutine(FadeOutClip());
+        StartCoroutine(LoadNextClip());
+    }
+
+    private IEnumerator FadeOutClip()
+    {
+        while (audioSource.volume > 0.1f)
+        {
+            audioSource.volume = Mathf.SmoothDamp(audioSource.volume, 0f, ref velocity, smoothTime);
+            yield return new WaitForEndOfFrame();
+        }
+        audioSource.volume = 0f;
+    }
+
+    private IEnumerator LoadNextClip()
+    {
+        yield return new WaitForSeconds (smoothTime + delayTime);
+        audioSource.clip = nextClip;
+        audioSource.volume = maxVolume;
     }
 }
