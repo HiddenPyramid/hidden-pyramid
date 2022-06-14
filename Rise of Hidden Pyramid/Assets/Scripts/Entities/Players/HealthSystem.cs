6d482b4f883c6c0788bb70c123e270c9aeff308b
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-06-14 23:01:04 +0200

    Minor changes

--- Rise of Hidden Pyramid/Assets/Scripts/Entities/Players/HealthSystem.cs
+++ Rise of Hidden Pyramid/Assets/Scripts/Entities/Players/HealthSystem.cs
@@ -18,6 +18,7 @@ public class HealthSystem : MonoBehaviour
     public float playerDieTime = 4.2f;
     private bool hasDied = false;
     public Animator curtainAnimator;
+    public Animator keepOnAnimator;
     private float timeToCloseCurtain = 3f;
 
     public ParticleSystem particleSystem;
@@ -58,12 +59,13 @@ public class HealthSystem : MonoBehaviour
         {
             Instantiate(particleSystem, transform.position, transform.rotation, null);
         }
-
-        yield return new WaitForSeconds(playerDieTime/2);
-
+        
         curtainAnimator.SetTrigger(Parameter.ANIM_REVIVES);
+        keepOnAnimator.gameObject.SetActive(true);
+        keepOnAnimator.SetBool("respawning", true);
 
         yield return new WaitForSeconds(timeToCloseCurtain);
+        keepOnAnimator.SetBool("respawning", false);
 
         playerAnimator.SetTrigger(Parameter.ANIM_REVIVES);
         shadowPlayerAnimator.SetTrigger(Parameter.ANIM_REVIVES);
@@ -73,11 +75,12 @@ public class HealthSystem : MonoBehaviour
         //Camera.main.transform.rotation = transform.rotation;
         PlayerStats.Health = Lives;
             
-        
-
         playerController.RegainLives();
         hasDied = false;
         GetComponent<PlayerMovement>().UnblockMove();
+        
+        yield return new WaitForSeconds(1f);
+        keepOnAnimator.gameObject.SetActive(false);
     }
 
 }
